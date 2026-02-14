1b57a13564ada5368f5a6063ed4ad7c1d5c6977f
jduhking <jamesbodebiyi@gmail.com>
2026-02-14 15:53:21 -0600

    Added routes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -12,6 +12,33 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/home")
+async def redirect_home():
+    return RedirectResponse(url="/")
+
+@app.get("/about", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
+
+@app.get("/projects", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("projects.html", {"request": request})
+
+
+
+@app.get("/blog", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("blog.html", {"request": request})
+
+
+
+@app.get("/games", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("games.html", {"request": request})
+
 
 
 
